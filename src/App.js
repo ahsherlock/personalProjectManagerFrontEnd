3d7c0aa387802a89de865153e2d7908534d5643a
Alec Sherlock <ahsherlock0@gmail.com>
2020-03-15 20:01:02 -0700

    added navbar,projectitem, and some css.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
-
+import Header from './components/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Import the bootstrap css file into main App.js
 function App() {
   return (
     <div className="App">
-    <Dashboard/>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to my application</h1>
-        <h2>This is the Dashboard screen from Dashboard.js</h2>
-      </div>
+        <div class="projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="display-4 text-center">Projects</h1>
+                    <br />
+                    <a href="ProjectForm.html" class="btn btn-lg btn-info">
+                        Create a Project
+                    </a>
+                    <br />
+                    <hr />
+                    <ProjectItem/>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        
     );
   }
 }
