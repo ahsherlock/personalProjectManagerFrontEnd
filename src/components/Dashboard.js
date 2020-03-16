156b0f7ea96d4bd418a867cd812a112ab633aa8d
Alec Sherlock <ahsherlock0@gmail.com>
2020-03-15 19:12:42 -0700

    adding first component: Dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my application</h1>
+        <h2>This is the Dashboard screen from Dashboard.js</h2>
+      </div>
+    );
+  }
+}
+export default Dashboard;
