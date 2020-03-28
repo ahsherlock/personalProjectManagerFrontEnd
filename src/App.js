b3cd40c9d188b1b802380b3899d991d62f35c030
Alec Sherlock <ahsherlock0@gmail.com>
2020-03-28 16:36:45 -0700

    created Redux store

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import Header from './components/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'; //Import the bootstrap css file into main App.js
 import { BrowserRouter as Router, Route } from 'react-router-dom'; //import and use Router/Route as shorthand.
 import AddProject from './components/Project/AddProject';
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/Dashboard" component={Dashboard} />
-        <Route exact path="/AddProject" component={AddProject} />
-      </div>
-    </Router>
-  );
+import { Provider } from 'react-redux';
+import store from './store';
+class App extends Component() {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/Dashboard" component={Dashboard} />
+            <Route exact path="/AddProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
