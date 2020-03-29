7a6c71bfc2d1459c4bd9e2282fdac69a555cdf32
Alec Sherlock <ahsherlock0@gmail.com>
2020-03-28 17:26:09 -0700

    added create project reducer and actions. it actually does the thing now.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'; //import and 
 import AddProject from './components/Project/AddProject';
 import { Provider } from 'react-redux';
 import store from './store';
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <Provider store={store}>
---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+//Defines types of actions 
+export const GET_ERRORS = "GET_ERRORS"
